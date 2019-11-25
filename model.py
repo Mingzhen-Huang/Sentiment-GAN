92581c4ffbe3b98f22ed3dd714e04b15cf1149a5
Mingzhen Huang <mingzhenhuang@gmail.com>
2019-11-24 21:14:18 -0500

    change F to nn

--- model.py
+++ model.py
@@ -14,8 +14,6 @@ class Generator(nn.Module):
     def __init__(self, input_size, sentence_len, vocab_size, hidden_size, num_layers, embedding_dim, dropout):
         super(Generator, self).__init__()
 
-
-
         self.noise = torch.rand(input_size, hidden_size)
         self.vocab_size = vocab_size
         self.model = nn.Sequential(
@@ -26,7 +24,7 @@ class Generator(nn.Module):
 
     def forward(self):     
         result = self.model(self.noise).int()
-        mask = torch.where(result > 0 && result < self.vocab_size, torch.tensor([1.]), torch.tensor([0.]))
+        mask = torch.where((result > 0 and result < self.vocab_size), torch.tensor([1.]), torch.tensor([0.]))
         return result * mask
 
 
@@ -35,16 +33,14 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()        
         self.embeddings = embeddings
 
-# batch * sentence_len
-
         self.model = nn.Sequential(
             nn.GRU(input_size = input_size, hidden_size = hidden_size, num_layers = num_layers, dropout = dropout),
             nn.Linear(input_size, 1),
-            F.sigmoid(),
+            nn.sigmoid(),
         )
 
     def forward(self, inputs):
-    	inputs = torch.tensor(inputs,dtype=torch.long)
-        mask = torch.where(inputs > 0, torch.tensor([1.]), torch.tensor([0.]))
+        inputs = torch.tensor(inputs,dtype=torch.long)
+        # mask = torch.where(inputs > 0, torch.tensor([1.]), torch.tensor([0.]))
         word_embed = self.embeddings(inputs)
         return self.model(word_embed)
