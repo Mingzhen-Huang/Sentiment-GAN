e4fcc741b377c65bd9671635512fc2c3afb07e92
shengwei.li <lucasleesw9@gmail.com>
2019-12-09 00:14:44 -0500

    new data

--- poetry_data.py
+++ poetry_data.py
@@ -7,7 +7,7 @@ import pandas as pd
 #     text = f.readlines()
 #     new_text = []
 #     for i in text:
-#         if len(i) > 5:
+#         if len(i) > 8:
 #             new_text.append(i)
 #     with open('newpoetry.txt','w') as newf:
 #         newf.writelines(new_text)
@@ -17,7 +17,7 @@ poems_df = pd.read_fwf('newpoetry.txt')
 poems_df['s'] = poems_df['s'].astype('str')
 
 
-data_lm = (TextList.from_df(poems_df.loc[:50000,:],cols='s')
+data_lm = (TextList.from_df(poems_df.loc[:50000],cols='s')
 	           .split_by_rand_pct(0.1)
 	           .label_for_lm()
 	           .databunch(bs=64))
