842626ff74de22303c6125a94fe899900a49b935
Mingzhen Huang <mingzhenhuang@gmail.com>
2019-12-09 08:20:46 -0500

    modify poetry reader

--- poetry_data.py
+++ poetry_data.py
@@ -3,23 +3,39 @@ from fastai.text import *
 import pandas as pd
 
 
-# with open('data/poetry.txt','r') as f:
+# with open('newpoetry.txt','r') as f:
 #     text = f.readlines()
 #     new_text = []
+#     count = 0
 #     for i in text:
-#         if len(i) > 8:
-#             new_text.append(i)
-#     with open('newpoetry.txt','w') as newf:
-#         newf.writelines(new_text)
+#         new_text.append(i)
+#         count+=1
+#         if count == 14:
+#         	count=0
+#         	new_text.append('\n')
+#         	new_text.append('\n')
+#     with open('newpoetrys.txt','w') as nf:
+#         nf.writelines(new_text)
 
-poems_df = pd.read_fwf('newpoetry.txt')
+# poems_df = pd.read_fwf('newpoetry.txt')
 
-poems_df['s'] = poems_df['s'].astype('str')
+# poems_df['s'] = poems_df['s'].astype('str')
+# print(poems_df)
 
+# data_lm = (TextList.from_df(poems_df.loc[:50000],cols='s')
+# 	           .split_by_rand_pct(0.1)
+# 	           .label_for_lm()
+# 	           .databunch(bs=64))
 
-data_lm = (TextList.from_df(poems_df.loc[:50000],cols='s')
-	           .split_by_rand_pct(0.1)
-	           .label_for_lm()
-	           .databunch(bs=64))
-
-data_lm.save('./data/poems_tmp')
+# data_lm.save('./data/poems_tmp')
+path = Path('./')
+bs = 64
+poems = (path/'newpoetrys.txt').open().read().split('\n\n')
+poems_df = pd.DataFrame(poems)
+tokenizer = Tokenizer(SpacyTokenizer, 'en')
+processor = [TokenizeProcessor(tokenizer=tokenizer), NumericalizeProcessor(min_freq=1,max_vocab=60000)]
+data_lm = (TextList.from_df(poems_df,path,cols=0,processor=processor)
+            .split_by_rand_pct(0.1)
+            .label_for_lm()           
+            .databunch(bs=bs))
+data_lm.save('poems_tmp')
